fa267ad1e70e220143b9d3002f89360f1f5faa3c
Luminocents <Jorda25365@live.ytech.edu>
2023-08-30 11:19:21 -0400

    Python to Javascript
    So I can work on it at home if I want to

---
+++ docpac_02010923/Python to Javascript/javascript.js
@@ -0,0 +1,34 @@
+/*function getMultipleRandom(colors, num) {
+    const shuffled = [...colors].sort(() => 0.5 - Math.random());
+
+    return shuffled.slice(0, num);
+}
+
+let colors = [
+    'Red', 'Green', 'Blue', 'Purple', 'Yellow', 'Black'
+];
+
+color = getMultipleRandom(colors, 2);
+console.log(color);
+console.log(color.indexOf('Blue'))
+if (color.indexOf('Blue') != -1 || color.indexOf('Green') != -1) {
+    console.log("Im Grue Dabudi Dabudie")
+}
+else if (color.indexOf('Black') !== -1 && color.indexOf('Yellow') != -1) {
+    console.log('Black and Yellow, Black and Yellow, Black and Yellow, Black and Yellow')
+}
+else {
+    console.log("If I was literally anything else I would die")
+};*/
+
+
+
+let amongus = ['The', 'Impostor', 'Sus']
+console.log(amongus)
+
+for (var i = 0; 1 < Array.length; i++) {
+    if (arr[i] == 'Impostor') {
+        arr.splice(i, 1);
+    }
+}
+console.log(amongus)
---
+++ docpac_02010923/Python to Javascript/python.py
@@ -0,0 +1,26 @@
+"""import random
+
+colors = ['Red', 'Green', 'Blue', 'Purple', 'Yellow', 'Black']
+
+color = random.sample(colors, 2)
+print(color)
+
+if 'Blue' in color or 'Green' in color: (
+    print("Im Grue Dabudi Dabudie")
+)
+elif 'Black' in color and 'Yellow' in color: (
+    print('Black and Yellow, Black and Yellow, Black and Yellow, Black and Yellow')
+)
+else: (
+    print('If I was literally anything else I would die')
+)"""
+
+
+
+amongus = ['The', 'Impostor', 'Sus']
+print(amongus)
+
+
+
+amongus.pop(0)
+print(amongus)
