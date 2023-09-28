9b772221f50d3f71427e92ae52830784a96fb356
Luminocents <Jorda25365@live.ytech.edu>
2023-09-28 14:58:27 -0400

    Last change I promise
    I made the 2 endpoints

--- docpac_06290923/neepractice/JordanSutherland/index.js
+++ docpac_06290923/neepractice/JordanSutherland/index.js
@@ -8,6 +8,11 @@ const PORT = 3000;
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
+    res.render('root.ejs')
+
+});
+
+app.get('/query', (req, res) => {
     if (!req.query.name) { res.render('index.ejs', { name: 'Guest' }) }
     else { res.render('index.ejs', { name: req.query.name }) }
 
