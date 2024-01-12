353ef2359baf7aa456d0bc94723c2ec4fe629099
isabellafeliciano1 <isabe25578@live.ytech.edu>
2024-01-12 09:24:05 -0500

    Changed the localhost to IP address

--- docpac_19120124/Websockets Project/IsabellaFeliciano/Server/Public/app.js
+++ docpac_19120124/Websockets Project/IsabellaFeliciano/Server/Public/app.js
@@ -1,4 +1,4 @@
-const socket = io('ws://localhost:3500')
+const socket = io('ws://172.16.3.104:3500')
 
 const msgInput = document.querySelector('#message')
 const nameInput = document.querySelector('#name')
