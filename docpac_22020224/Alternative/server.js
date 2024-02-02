0a3e26b91042979bdcc277a7ea051c797b04e7ad
Taysaun <taysa25495@live.ytech.edu>
2024-02-02 08:15:21 -0500

    Alternative changes

--- docpac_22020224/Alternative/server.js
+++ docpac_22020224/Alternative/server.js
@@ -1,13 +1,65 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+// const express = require("express")
+// const app = express()
+// const io = require("socket.io")(3000)
+// const http = require("http").Server(app)
 
-app.set("view engine", "ejs")
+// //start http server
+// // http.listen(3000, () => {
+// //     console.log("server running on port 3000")
+// // })
+
+
+// app.use(express.static("public"))
+
+// io.on("connection", (socket) => {
+//     console.log("new user connected")
+//     socket.emit("message", "Welcome to the chat")
+//     socket.broadcast.emit("message", "A new user has joined the chat")
+    
+//     socket.on("sendMessage", (message) => {
+//         io.emit("message", message)
+//     })
+    
+//     socket.on("disconnect", () => {
+//         io.emit("message", "A user has left the chat")
+//     })
+// })
+
+// app.set("view engine", "ejs")
+
+// app.get("/", (req, res) => {
+//     res.render("index")
+// })
+
+
+
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+    console.log("new user connected");
+    socket.emit("message", "Welcome to the chat");
+    socket.broadcast.emit("message", "A new user has joined the chat");
+
+    socket.on("sendMessage", (message) => {
+        io.emit("message", message);
+    });
+
+    socket.on("disconnect", () => {
+        io.emit("message", "A user has left the chat");
+    });
+});
+
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.render("index")
-})
+    res.render("index");
+});
 
-app.listen(3000, (err)=>{
+http.listen(3000, (err)=>{
     if (err) {
         console.log(err)
     } else {
